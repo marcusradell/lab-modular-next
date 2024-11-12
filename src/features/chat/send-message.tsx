a273c3c3e584065675d210ac908df62efaec824a
Marcus Rådell <marcus@radell.net>
2024-11-12 09:42:43 +0100

    improve onClick handler name

--- src/features/chat/send-message.tsx
+++ src/features/chat/send-message.tsx
@@ -3,11 +3,11 @@
 import { sendMessageAction } from "./actions";
 
 export function SendMessage() {
-  async function onSend() {
+  async function onSendMessage() {
     console.log("Inside onSend");
 
     await sendMessageAction({ content: "Hi!" });
   }
 
-  return <button onClick={onSend}>Send</button>;
+  return <button onClick={onSendMessage}>Send</button>;
 }
